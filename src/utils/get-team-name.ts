d3151b35d6824d0eefb8908ed69fba567f272cb2
Davi Hasson Castro <davihasson@gmail.com>
2024-08-16 11:50:14 -0300

    feat: adding ligue1 teams remapping

--- src/utils/get-team-name.ts
+++ src/utils/get-team-name.ts
@@ -1,22 +1,11 @@
 const teamNameMap: { [key: string]: string } = {
-  Augsburg: "Augsburg",
   "Bayer Leverkusen": "Leverkusen",
-  Bayern: "Bayern",
-  Bochum: "Bochum",
   "Borussia Dortmund": "Dortmund",
   "Borussia Mönchengladbach": "Mgladbach",
   "Eintracht Frankfurt": "Frankfurt",
-  Freiburg: "Freiburg",
-  Heidenheim: "Heidenheim",
-  Hoffenheim: "Hoffenheim",
-  "Holstein Kiel": "Holstein Kiel",
-  "Mainz 05": "Mainz 05",
-  "RB Leipzig": "RB Leipzig",
-  "St. Pauli": "St. Pauli",
-  Stuttgart: "Stuttgart",
-  "Union Berlin": "Union Berlin",
   "Werder Bremen": "W. Bremen",
-  Wolfsburg: "Wolfsburg",
+  "Paris Saint Germain": "PSG",
+  "Saint-Etienne": "St. Etienne",
 };
 
 export function getTeamName(inputName: string): string {
